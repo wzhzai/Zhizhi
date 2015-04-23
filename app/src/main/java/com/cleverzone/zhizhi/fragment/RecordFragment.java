7f3bd07d9a2eee7859e90ca42c7867abe494e3bb
wzhzai <wzhz3000@163.com>
2015-04-23 21:01:28 +0800

    add calendar

--- app/src/main/java/com/cleverzone/zhizhi/fragment/RecordFragment.java
+++ app/src/main/java/com/cleverzone/zhizhi/fragment/RecordFragment.java
@@ -6,8 +6,15 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.cleverzone.zhizhi.R;
+import com.cleverzone.zhizhi.comui.KCalendar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +26,8 @@ public class RecordFragment extends BaseFragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    String mDate = null;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -63,5 +72,90 @@ public class RecordFragment extends BaseFragment {
         return inflater.inflate(R.layout.fragment_record, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+
+        final TextView popupwindow_calendar_month = (TextView) view
+                .findViewById(R.id.popupwindow_calendar_month);
+        final KCalendar calendar = (KCalendar) view
+                .findViewById(R.id.popupwindow_calendar);
+
+        popupwindow_calendar_month.setText(calendar.getCalendarYear() + "年"
+                + calendar.getCalendarMonth() + "月");
+
+        if (null != mDate) {
+
+            int years = Integer.parseInt(mDate.substring(0,
+                    mDate.indexOf("-")));
+            int month = Integer.parseInt(mDate.substring(
+                    mDate.indexOf("-") + 1, mDate.lastIndexOf("-")));
+            popupwindow_calendar_month.setText(years + "年" + month + "月");
+
+            calendar.showCalendar(years, month);
+            calendar.setCalendarDayBgColor(mDate, R.drawable.calendar_date_focused);
+        }
+
+        List<String> list = new ArrayList<String>(); //设置标记列表
+        list.add("2014-04-01");
+        list.add("2014-04-02");
+        calendar.addMarks(list, 0);
+
+        //监听所选中的日期
+        calendar.setOnCalendarClickListener(new KCalendar.OnCalendarClickListener() {
+
+            public void onCalendarClick(int row, int col, String dateFormat) {
+                int month = Integer.parseInt(dateFormat.substring(
+                        dateFormat.indexOf("-") + 1,
+                        dateFormat.lastIndexOf("-")));
+
+                if (calendar.getCalendarMonth() - month == 1//跨年跳转
+                        || calendar.getCalendarMonth() - month == -11) {
+                    calendar.lastMonth();
+
+                } else if (month - calendar.getCalendarMonth() == 1 //跨年跳转
+                        || month - calendar.getCalendarMonth() == -11) {
+                    calendar.nextMonth();
+
+                } else {
+                    calendar.removeAllBgColor();
+                    calendar.setCalendarDayBgColor(dateFormat, R.drawable.calendar_date_focused);
+                    mDate = dateFormat;//最后返回给全局 mDate
+                }
+            }
+        });
+
+        //监听当前月份
+        calendar.setOnCalendarDateChangedListener(new KCalendar.OnCalendarDateChangedListener() {
+            public void onCalendarDateChanged(int year, int month) {
+                popupwindow_calendar_month
+                        .setText(year + "年" + month + "月");
+            }
+        });
+
+        //上月监听按钮
+        RelativeLayout popupwindow_calendar_last_month = (RelativeLayout) view
+                .findViewById(R.id.popupwindow_calendar_last_month);
+        popupwindow_calendar_last_month
+                .setOnClickListener(new View.OnClickListener() {
+
+                    public void onClick(View v) {
+                        calendar.lastMonth();
+                    }
+
+                });
+
+        //下月监听按钮
+        RelativeLayout popupwindow_calendar_next_month = (RelativeLayout) view
+                .findViewById(R.id.popupwindow_calendar_next_month);
+        popupwindow_calendar_next_month
+                .setOnClickListener(new View.OnClickListener() {
+
+                    public void onClick(View v) {
+                        calendar.nextMonth();
+                    }
+                });
+
+    }
 }
--- app/src/main/java/com/cleverzone/zhizhi/fragment/ZhizhiFragment.java
+++ app/src/main/java/com/cleverzone/zhizhi/fragment/ZhizhiFragment.java
@@ -1,11 +1,16 @@
 package com.cleverzone.zhizhi.fragment;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.cleverzone.zhizhi.R;
 
@@ -23,6 +28,7 @@ public class ZhizhiFragment extends BaseFragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Context mContext;
 
 
     /**
@@ -59,9 +65,70 @@ public class ZhizhiFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_zhizhi, container, false);
     }
 
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mContext = activity;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ListView listView = (ListView) view.findViewById(R.id.zhizhi_list);
+        listView.addHeaderView(initHeaderView());
+        listView.setAdapter(new ZhizhiListAdapter());
+    }
+
+    private View initHeaderView() {
+        return LayoutInflater.from(mContext).inflate(R.layout.zhizhi_list_hint_header, null);
+    }
+
+    private class ZhizhiListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(mContext).inflate(R.layout.list_item_zhizhi_list, parent, false);
+            }
+
+            ImageView ivBigCycle = (ImageView) convertView.findViewById(R.id.zhizhi_item_big_cycle);
+            ImageView ivLastBigCycle = (ImageView) convertView.findViewById(R.id.zhizhi_item_last_big_cycle);
+            int paddingLeft = convertView.getPaddingLeft();
+            int paddingRight = convertView.getPaddingRight();
+            if (position == 0) {
+                ivBigCycle.setVisibility(View.VISIBLE);
+                convertView.setPadding(paddingLeft, mContext.getResources().getDimensionPixelSize(R.dimen.zhizhi_list_item_padding_left_right), paddingRight, 0);
+            } else {
+                ivBigCycle.setVisibility(View.GONE);
+                convertView.setPadding(paddingLeft, 0, paddingRight, 0);
+            }
+
+            if (position == getCount() - 1) {
+                ivLastBigCycle.setVisibility(View.VISIBLE);
+            } else {
+                ivLastBigCycle.setVisibility(View.GONE);
+            }
+
+            return convertView;
+        }
+    }
 
 }
